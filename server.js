af43ff99bb9a7a3ddfe0f3019c4a7312bc7b7afd
jangtaehee <jangth0655@gmail.com>
2024-01-18 14:10:32 +0900

    first

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`${port}에서 응답을 기다리는 중....`);
+});
